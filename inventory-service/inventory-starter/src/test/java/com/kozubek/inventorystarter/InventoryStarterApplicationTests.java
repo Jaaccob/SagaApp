e9695a9d86ccc520052f176259dd5d699089385b
Jacob <kozubekjacob@gmail.com>
2024-12-06 18:42:41 +0100

    001 - create common service with common controller

---
+++ inventory-service/inventory-starter/src/test/java/com/kozubek/inventorystarter/InventoryStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kozubek.inventorystarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
